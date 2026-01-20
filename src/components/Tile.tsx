be88a1635dd2610a917cf5fe515071116fa05576
DavidDup0nt <david.dupont.eseo@gmail.com>
2026-01-20 16:13:03 +0100

    Update Tile.tsx
    border '[ -webkit-text-stroke:2px_white ]

--- src/components/Tile.tsx
+++ src/components/Tile.tsx
@@ -120,7 +120,7 @@ export default function Tile({ tile, small = false }: { tile: TileCode | '00'; s
 			  className={clsx(
 				'absolute top-1 right-1 text-xs font-bold justify-right',
 				'px-1 rounded',
-					'bg-white px-5 rounded',
+					'[ -webkit-text-stroke:2px_white ] dark:[ -webkit-text-stroke:2px_black ]'
 					color === 'red'
 						? 'text-red-600 dark:text-red-700'
 						: color === 'green'
