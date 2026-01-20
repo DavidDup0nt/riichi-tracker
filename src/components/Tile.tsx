8681e8041f094dd79a8e3d812a29dab0e37c1872
DavidDup0nt <david.dupont.eseo@gmail.com>
2026-01-20 16:22:51 +0100

    Update Tile.tsx
    text right

--- src/components/Tile.tsx
+++ src/components/Tile.tsx
@@ -118,9 +118,9 @@ export default function Tile({ tile, small = false }: { tile: TileCode | '00'; s
 		 
 			<span
 			  className={clsx(
-				'absolute top-1 right-1 text-xs font-bold justify-right',
+				'absolute top-1 right-1 text-xs font-bold text-right',
 				'px-1 rounded',
-					'[ -webkit-text-stroke:1px_white ] dark:[ -webkit-text-stroke:1px_black ]'
+					'[ -webkit-text-stroke:1px_white ] dark:[ -webkit-text-stroke:1px_black ]',
 					color === 'red'
 						? 'text-red-600 dark:text-red-700'
 						: color === 'green'
